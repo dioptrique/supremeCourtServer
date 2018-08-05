9b29a69a6fcde0afafb319ea49b18a02e71e9149
dioptrique <h0910139@gmail.com>
2018-08-05 11:16:09 +0800

    Added .env for dev

---
+++ config/config.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DEV_DB,
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  production: {
+    username: ,
+    password: ,
+    database: ,
+    host: ,
+    port: ,
+    dialect: 'postgres'
+  }
+};
