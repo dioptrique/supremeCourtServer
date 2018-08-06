deb5cb4f58aec47887d970b3f883d88d5e2ad727
dioptrique <h0910139@gmail.com>
2018-08-07 02:54:43 +0800

    experimenting with promises

--- server/controllers/phoneNoToRegistrationId.js
+++ server/controllers/phoneNoToRegistrationId.js
@@ -70,7 +70,7 @@ const sendNotifications = (req, res) => {
         'Authorization': 'key=AAAATBUSzKs:APA91bFDorxTw-AXVrFTGhVEtnobQHRLQ2g8pHJqnw5fDwMiFBKPS6kBgatdWDBdKHwnpszMMxzhltpAvvML97Kn6QXSRTQh5dADQ7EUirzQdxfEHAfhmOu1e0IHc-WrKroIOi7Xz6K4c2PUP1gq_El75ppfIHepXw',
         'project_id':'326771068075'
       },
-      data: {
+      params: {
         operation: 'create',
         notification_key_name: uuidv1(),
         registration_ids: registrationIds
