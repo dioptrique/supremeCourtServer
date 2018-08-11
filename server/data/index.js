c1be4f8715a032a007b2d8966528a8aa70d929bd
dioptrique <h0910139@gmail.com>
2018-08-11 14:16:43 +0800

    Added hearing data

--- server/data/index.js
+++ server/data/index.js
@@ -2,9 +2,10 @@ var rawData = require('./data1')
 
 var array = []
 rawData.forEach((hearingJson) => {
-  array.push([hearingJson.hearingId, hearingJson]);
+  array.push([hearingJson.HearingID, hearingJson]);
 })
 
 var hearingIdToHearing = new Map(array)
+console.log('Test map: '+hearingIdToHearing.get('296267').Date)
 
 module.exports = hearingIdToHearing
