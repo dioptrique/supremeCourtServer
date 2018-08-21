197640610811a67c77222e96da1e0468c364ac6a
dioptrique <h0910139@gmail.com>
2018-08-22 01:33:03 +0800

    changed firebase link to short link with api call

--- server/helpers/sendSMS.js
+++ server/helpers/sendSMS.js
@@ -1,6 +1,7 @@
 var axios = require('axios')
 var constants = require('constants')
 
+
 /**
  * @function sendSMS
  * @summary: Send SMS to the device group using sms gateway
@@ -23,16 +24,35 @@ module.exports = sendSMS = (hearingId, phoneNos, message) => {
   }
   //Manually create a firebase dynamic link to our application
   var firebaseLink = 'https://supremecourtbook.page.link/?link=https://supremecourtbook.com/'+hearingId+'&apn=com.example.skynet.supremecourt'
-
-  var augmentedMessage = message + ' Click here: '+firebaseLink;
-  
-  const url = 'https://www.isms.com.my/isms_send.php?un='+ISMS_USER+'&pwd='+ISMS_PWD+'&dstno='+phoneNosInUrl+'&msg='+augmentedMessage+'&type=1&sendid=12345678'
-  return axios({
-    method: 'get',
-    url: url
+  // Make post request to get a short url link to bring user to the scheduling activity
+  axios({
+    method:'post',
+    url:'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyBdrdtILupHd4pZzxFkTSyuccilYEBN_uY',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    data:{
+        "longDynamicLink": "https://supremecourtbook.page.link/?link=https://www.supremecourtbook.com/"+hearingId+"&apn=com.example.skynet.supremecourt",
+        "suffix": {
+          "option": "SHORT"
+        }
+    }
   })
-  .then(() => {console.log('Smses sent!')})
-  .catch((err) => {
+  .then((response) => {
+    var firebaseLink = response.data.shortLink;
+    var augmentedMessage = message + ' Click here to sign up: '+firebaseLink;
+
+    const url = 'https://www.isms.com.my/isms_send.php?un='+ISMS_USER+'&pwd='+ISMS_PWD+'&dstno='+phoneNosInUrl+'&msg='+augmentedMessage+'&type=1&sendid=12345678'
+    return axios({
+      method: 'get',
+      url: url
+    })
+    .then(() => {console.log('Smses sent!')})
+    .catch((err) => {
+      throw err;
+    })
+  })
+  .catch(err => {
     throw err;
   })
 }
